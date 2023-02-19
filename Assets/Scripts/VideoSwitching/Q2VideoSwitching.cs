216ea76ec1e8c582cd221fc9c4658dd398218d49
Kenneth-Galvez-BSIT3N <57065417+PalindromeRotator@users.noreply.github.com>
2023-02-19 14:06:57 +0800

    Initial Commit v2

--- Assets/Scripts/VideoSwitching/Q2VideoSwitching.cs
+++ Assets/Scripts/VideoSwitching/Q2VideoSwitching.cs
@@ -5,41 +5,41 @@ using UnityEngine.SceneManagement;
 
 public class Q2VideoSwitching : MonoBehaviour
 {
-   // VideoQ1 //
-   public void VideoPlaying1()
-   {
-    SceneManager.LoadScene("#1VideoPlayingQ2");
-   }
-   public void VideoPlaying2()
-   {
-    SceneManager.LoadScene("#1VideoPlayingQ2");
-   }
-   public void VideoPlaying3()
-   {
-    SceneManager.LoadScene("#3VideoPlayingQ2");
-   }
-      public void VideoPlaying4()
-   {
-    SceneManager.LoadScene("#4VideoPlayingQ2");
-   }
-      public void VideoPlaying5()
-   {
-    SceneManager.LoadScene("#5VideoPlayingQ2");
-   }
-      public void VideoPlaying6()
-   {
-    SceneManager.LoadScene("#6VideoPlayingQ2");
-   }
-      public void VideoPlaying7()
-   {
-    SceneManager.LoadScene("#7VideoPlayingQ2");
-   }
-      public void VideoPlaying8()
-   {
-    SceneManager.LoadScene("#8VideoPlayingQ2");
-   }
-      public void VideoPlaying9()
-   {
-    SceneManager.LoadScene("#9VideoPlayingQ2");
-   }
+    // VideoQ1 //
+    public void VideoPlaying1()
+    {
+        SceneManager.LoadScene("#1VideoPlayingQ2");
+    }
+    public void VideoPlaying2()
+    {
+        SceneManager.LoadScene("#2VideoPlayingQ2");
+    }
+    public void VideoPlaying3()
+    {
+        SceneManager.LoadScene("#3VideoPlayingQ2");
+    }
+    public void VideoPlaying4()
+    {
+        SceneManager.LoadScene("#4VideoPlayingQ2");
+    }
+    public void VideoPlaying5()
+    {
+        SceneManager.LoadScene("#5VideoPlayingQ2");
+    }
+    public void VideoPlaying6()
+    {
+        SceneManager.LoadScene("#6VideoPlayingQ2");
+    }
+    public void VideoPlaying7()
+    {
+        SceneManager.LoadScene("#7VideoPlayingQ2");
+    }
+    public void VideoPlaying8()
+    {
+        SceneManager.LoadScene("#8VideoPlayingQ2");
+    }
+    public void VideoPlaying9()
+    {
+        SceneManager.LoadScene("#9VideoPlayingQ2");
+    }
 }
